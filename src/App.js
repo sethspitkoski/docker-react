655317bd97d67134ba9ace4a7e8752c524617328
sethspitkoski <seth.spitkoski@gmail.com>
2019-06-15 20:48:38 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Coutts is a bitch
+          Coutts is a bitch!
         </p>
         <a
           className="App-link"
